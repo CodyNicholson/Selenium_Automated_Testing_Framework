c30ee89b3908290c8f57209154335415accd0054
Cody Nicholson <codynicholson96@gmail.com>
2017-04-19 20:06:37 -0500

    Found a way to get the path to the project locally

--- src/Users/strategies/codyStrategy.java
+++ src/Users/strategies/codyStrategy.java
@@ -7,7 +7,7 @@ public class codyStrategy implements userStrategyClass {
     // Chrome Web Application
     private String chromeApplicationPath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
     private String IEApplicationPath = "C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe";
-    private String testResultsPath = "src\\Results\\Test_Results\\";
+    private String testResultsPath = "src\\Results\\Test_Results\\"; // System.getProperty("user.dir")
 
     @Override
     public String getChromeApplicationPath()
